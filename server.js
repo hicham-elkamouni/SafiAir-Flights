6532ea3fa10cede96607ee19dfccc02ef38d8675
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2021-12-14 16:31:00 +0100

    folder structure done 100% and a and ability to retrieve data from database

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 const http = require("http");
 // const fs = require("fs");
+const url = require("url");
 const _ = require("lodash");
 
-const routes = require("./routes");
+const routes = require("./routes/router");
 
-const server = http.createServer(routes);
+const server = http.createServer(function (req, res){
+
+    let parsedURL = url.parse(req.url, true);
+    let path = parsedURL.pathname;
+    path = path.replace(/^\/+|\/+$/g, "");
+
+    if (path == "") {
+        path = "index";
+    } else {
+        path = path.split("/")[0]
+    }
+    console.log(path);
+
+    let route = typeof routes[path] !== "undefined" ? routes[path] : routes["404"];
+    console.log(route);
+
+    let data = {
+        path: path,
+        url: parsedURL.pathname,
+        req : req
+    }
+    
+    route(data, res, req);
+});
 
 server.listen(3000);
